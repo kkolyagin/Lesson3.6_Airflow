2dc26e6c77387e9b8b22451757138e3876838a98
unknown <kkolyagin@mail.ru>
2022-11-29 22:32:47 +0300

    add dags

--- dags/second_dag.py
+++ dags/second_dag.py
@@ -12,39 +12,40 @@ pg_pass = 'postgres'
 pg_db = 'test'
 
 def hello():
-    print("Hello")
-    
+    print("Hello!")
+
 def connect_to_psql():
     conn = psycopg2.connect(host=pg_hostname, port=pg_port, user=pg_username, password=pg_pass, database=pg_db)
     cursor = conn.cursor()
-    
+
     cursor.execute("CREATE TABLE test_table (id serial PRIMARY KEY, num integer, data varchar);")
     cursor.execute("INSERT INTO test_table (num, data) VALUES (%s, %s)",(100, "abc'def"))
-   
-    conn.commit()
     
+    #cursor.fetchall()
+    conn.commit()
+
     cursor.close()
     conn.close()
-    
+
 def read_from_psql():
     conn = psycopg2.connect(host=pg_hostname, port=pg_port, user=pg_username, password=pg_pass, database=pg_db)
     cursor = conn.cursor()
-    
-    cursor.execute("Select * from test_table;")
+
+    cursor.execute("SELECT * FROM test_table;")
     print(cursor.fetchone())
-        
+    
     cursor.close()
     conn.close()
 
 # A DAG represents a workflow, a collection of tasks
-with DAG(
-	dag_id="first_dag", start_date=datetime(2022, 1, 1), schedule = "0 0 * * *"
-) as dag:
-    #Task are represent as operators
+with DAG(dag_id="second_dag", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+    
+    # Tasks are represented as operators
     bash_task = BashOperator(task_id="hello", bash_command="echo hello")
     python_task = PythonOperator(task_id="world", python_callable = hello)
-    conn_to_psql_tsk = PythonOperator(task_id="connect_to_psql", python_callable = connect_to_psql)
-    read_from_psql_tsk =PythonOperator(task_id="read_from_psql", python_callable = read_from_psql)
+    conn_to_psql_tsk = PythonOperator(task_id="conn_to_psql", python_callable = connect_to_psql)
+    read_from_psql_tsk = PythonOperator(task_id="read_from_psql", python_callable = read_from_psql)
 
-    #Set dependencies between tasks
-    bash_task >> python_task>>conn_to_psql_tsk>>read_from_psql_tsk
+
+    # Set dependencies between tasks
+    bash_task >> python_task >> conn_to_psql_tsk >> read_from_psql_tsk
