3514d485571dd38f4a209d8cedfccacf70e48e23
unknown <kkolyagin@mail.ru>
2022-11-30 20:03:55 +0300

    add a

--- dags/practic_dag.py
+++ dags/practic_dag.py
@@ -4,11 +4,13 @@ import psycopg2
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+from airflow.operators.email_operator import EmailOperator
 
 pg_hostname = 'host.docker.internal'
 pg_port = '5430'
 pg_username = 'postgres'
-pg_pass = 'password'
+pg_pass = 'postgres'
 pg_db = 'test'
 
 def hello():
@@ -36,16 +38,27 @@ def read_from_psql():
     
     cursor.close()
     conn.close()
-
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="first_dag", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
     
+def sum1forline(filename):
+    with open(filename) as f:
+        return sum(1 for line in f)
+        
+def FileLineCount ():
+    filename = 'cities.csv'
+    path = '/home/airflow/'
+    with open(path+filename) as f:
+         linecount = sum(1 for line in f) 
+    print(linecount)
+         
+# A DAG represents a workflow, a collection of tasks
+with DAG(dag_id="test_dag", start_date=datetime(2022, 11, 30), schedule="0 0 * * *") as dag:    
     # Tasks are represented as operators
-    bash_task = BashOperator(task_id="hello", bash_command="echo hello")
-    python_task = PythonOperator(task_id="world", python_callable = hello)
-    conn_to_psql_tsk = PythonOperator(task_id="conn_to_psql", python_callable = connect_to_psql)
-    read_from_psql_tsk = PythonOperator(task_id="read_from_psql", python_callable = read_from_psql)
-
+    #bash_task = BashOperator(task_id="hello", bash_command="echo hello")
+    #python_task = PythonOperator(task_id="world", python_callable = hello)
+    #conn_to_psql_tsk = PythonOperator(task_id="conn_to_psql", python_callable = connect_to_psql)
+    #read_from_psql_tsk = PythonOperator(task_id="read_from_psql", python_callable = read_from_psql)
+    FileLineCount_tsk =PythonOperator(task_id="FileLineCount", python_callable = FileLineCount)
 
     # Set dependencies between tasks
-    bash_task >> python_task >> conn_to_psql_tsk >> read_from_psql_tsk
+    #bash_task >> python_task >> conn_to_psql_tsk >> read_from_psql_tsk
+    FileLineCount_tsk
