d1cb97ef0d6b2d23e3389ef3c3c820d3d6dce3be
unknown <kkolyagin@mail.ru>
2022-12-08 23:33:16 +0300

    same changed

--- dags/light_dag.py
+++ dags/light_dag.py
@@ -11,12 +11,12 @@ def hello():
     print("Hello!")
 
 def TwoNumsPrt():
-    filename='/home/airflow/test.txt'
+    filename='light.txt'
     with open(filename, 'a+') as f:
         f.write(f'{random.randint(0, 9)} {random.randint(0, 9)}\n')
         
 def TwoNumsCalc():
-    filename='/home/airflow/test.txt'
+    filename='light.txt'
     x=0
     output=''
     with open(filename, 'r') as f:
@@ -24,13 +24,14 @@ def TwoNumsCalc():
             str_inp = line.replace('\n','').split(' ')
             if len(str_inp) >= 2:
                 x += int(str_inp[0])-int(str_inp[1])
-                output += f'{str_inp[0]} {str_inp[1]} {str(x)}\n'    
-    print(output)            
-    with open(filename, 'r+') as f:
+                output += f'{str_inp[0]} {str_inp[1]}\n'    
+    output += f'{str(x)}\n' 
+    
+    with open(filename, 'w') as f:
         f.write(output)
     
 # A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="light_dag", start_date=datetime(2022, 12, 5), schedule="0-5 9 5 12 *") as dag:    
+with DAG(dag_id="light_dag", start_date=datetime(2022,12,8), schedule_interval="18-23 20 * * *", catchup = False) as dag:    
     # Tasks are represented as operators
     bash_task = BashOperator(task_id="hello", bash_command="echo hello", do_xcom_push=False)
     python_task = PythonOperator(task_id="world", python_callable = hello)
